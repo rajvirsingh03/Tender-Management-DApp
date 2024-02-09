96e21b0779bc1df0ebcc332ab31ff015c6b52941
kartik3110 <kartik31.gupta@gmail.com>
2024-02-10 03:08:14 +0530

    added pages and components

---
+++ client/src/pages/OpenBidPage.jsx
@@ -0,0 +1,7 @@
+export default function OpenBidPage() {
+  return (
+    <div>
+      <div>OpenBidPage</div>
+    </div>
+  );
+}
---
+++ client/src/pages/SelectiveBidPage.jsx
@@ -0,0 +1,7 @@
+export default function SelectiveBidPage() {
+  return (
+    <div>
+      <div>SelectiveBidPage</div>
+    </div>
+  );
+}
