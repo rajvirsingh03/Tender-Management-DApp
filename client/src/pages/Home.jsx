8e967d36dd2de426e5ff6b7cbe3a8be3655d881d
rajvirsingh03 <rsingh6_be21@thapar.edu>
2024-02-10 10:32:49 +0530

    Last Commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
         <div className="flex flex-col bg-base-300 h-full ml-3 rounded-xl justify-center items-center bg-opacity-80">
 
             <div className="text-4xl font-bold">
-                Welcome to D-Ender!
+                Welcome to our Tender DApp!
             </div>
             <div className="text-xl text-center font-semibold w-[700px] mt-5">
             Welcome to our decentralized tender management platform!<br/> We're revolutionizing the traditional tender process with transparency and fairness. Our user-friendly interface, secure communication, and blockchain technology ensure an equitable and efficient bidding process. Join us today and experience the power of decentralized tender management!
